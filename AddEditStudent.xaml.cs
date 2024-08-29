14d978b379ef90a1ff6cc36abf3df3146f8a9f35
Mikołaj <pzx112240@student.poznan.merito.pl>
2024-08-29 13:53:58 +0200

    Visual changes, adding new window.

---
+++ AddEditStudent.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StudentDiary
+{
+    /// <summary>
+    /// Logika interakcji dla klasy AddEditStudent.xaml
+    /// </summary>
+    public partial class AddEditStudent : Window
+    {
+        public AddEditStudent()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,6 @@ namespace StudentDiary
     /// </summary>
     public partial class App : Application
     {
+
     }
 }
